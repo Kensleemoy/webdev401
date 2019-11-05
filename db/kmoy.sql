4ea908c2adf77a821d9426712ecae29bc3d7189b
Kensleemoy <43187663+Kensleemoy@users.noreply.github.com>
2019-11-04 22:59:41 -0700

    Add files via upload

---
+++ db/kmoy.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user {
+    userId      INT  AUTO_INCREMENT    PRIMARY KEY,
+    name        VARCHAR(20)    NOT NULL,
+    username    VARCHAR(255)    NOT NULL,
+    password    VARCHAR(8)      NOT NULL
+}
+
